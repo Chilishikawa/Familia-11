a79b8d498efac979bf1525c49b60489d6ec8023d
Chilishikawa <chilishikawa13@gmail.com>
2025-04-20 23:32:04 -0400

    Carpinchito 1 - se agregan modulos publicidad para artes estadísticas y header Cianware - Noticias

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Animación del título principal
     const tituloPrincipal = document.getElementById('titulo-principal1');
     tituloPrincipal.style.opacity = 0;
     tituloPrincipal.style.transform = 'translateY(-20px)';
-    setTimeout(function() {
+    setTimeout(function () {
         tituloPrincipal.style.transition = 'all 1s ease-in-out';
         tituloPrincipal.style.opacity = 1;
         tituloPrincipal.style.transform = 'translateY(0)';
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const presentacionSection = document.getElementById('presentacion-section');
     presentacionSection.style.opacity = 0;
     presentacionSection.style.transform = 'scale(0.8)';
-    setTimeout(function() {
+    setTimeout(function () {
         presentacionSection.style.transition = 'all 1s ease-in-out';
         presentacionSection.style.opacity = 1;
         presentacionSection.style.transform = 'scale(1)';
@@ -24,10 +24,48 @@ document.addEventListener("DOMContentLoaded", function() {
     menuItems.forEach((item, index) => {
         item.style.opacity = 0;
         item.style.transform = 'translateX(-20px)';
-        setTimeout(function() {
+        setTimeout(function () {
             item.style.transition = 'all 0.5s ease-in-out';
             item.style.opacity = 1;
             item.style.transform = 'translateX(0)';
         }, 100 * index);
     });
 });
+
+const coloresWeb = [
+    "AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque",
+    "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue",
+    "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan",
+    "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki",
+    "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon",
+    "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet",
+    "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite",
+    "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray",
+    "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki",
+    "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral",
+    "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink",
+    "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue",
+    "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine",
+    "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue",
+    "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue",
+    "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive",
+    "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen",
+    "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum",
+    "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon",
+    "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue",
+    "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato",
+    "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen"
+];
+
+const letras = document.querySelectorAll(".letra-animada");
+
+function cambiarColor() {
+    letras.forEach(letra => {
+      const randomColor = coloresWeb[Math.floor(Math.random() * coloresWeb.length)];
+      letra.style.color = randomColor;
+    });
+  }
+
+
+// Cambiar color cada 1 segundo
+setInterval(cambiarColor, 1000);
